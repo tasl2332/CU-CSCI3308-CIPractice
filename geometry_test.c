2bf8ad7cf4b66b9b219e7ff405b394c20f8c396d
tasl2332 <tasl2332@colorado.edu>
2018-11-28 16:17:15 -0700

    addd tri area with test

--- geometry_test.c
+++ geometry_test.c
@@ -145,6 +145,20 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+START_TEST(test_2d_triangle_area){
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+
+    a.x = 6;
+    a.y = 6;
+    b.x = 0;
+    b.y = 0;
+    c.x = 7;
+    c.y = 2;
+    ck_assert(coord_2d_area_triangle(&a, &b, &c) == 15);
+}
+END_TEST
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -162,10 +176,14 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* tc_2d_triangle_area = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_2d_triangle_area, test_2d_triangle_area);
+
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, tc_2d_triangle_area);
 
     /* Return Suite */
     return s;
